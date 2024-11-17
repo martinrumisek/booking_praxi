5b6a6b3cf2bed97e7ac3cf7b21127e51abe7e30a
martinrumisek <mrumisek@seznam.cz>
2024-11-17 22:02:37 +0100

    Přidání komentáře pro routy.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,16 +5,19 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/home', 'Home::index');
-$routes->get('login', 'Home::login');
-$routes->get('registration', 'Home::registration');
-$routes->get('practise_offer', 'Home::offerView');
-$routes->get('people','Home::people');
-$routes->get('company','Home::companyView');
-$routes->get('profile','Home::profileView');
 
+// ROUTY PRO VIEWČKA
+$routes->get('/home', 'Home::index'); //routa na hlavní stránku
+$routes->get('login', 'Home::login'); //Přihlašovácí stránka
+$routes->get('registration', 'Home::registration'); //stránka pro registraci firmy
+$routes->get('practise_offer', 'Home::offerView'); //stránka pro nabídky praxe
+$routes->get('people','Home::people'); //stránka pro zobrazení lidí z oauh
+$routes->get('company','Home::companyView'); //stránka pro zobrazení firem
+$routes->get('profile','Home::profileView'); //stránka pro zobrazení profilu
+//ROUTY PRO BTN - SCRIPT
+//ROUTY PRO AUTH
 
-//Zkoušky
-$routes->get('logAD', 'Auth::loginOAUH');      
-$routes->get('/', 'Auth::callback');   
-$routes->get('azure-users', 'UserAzureSync::index');
+//ZKUŠEBNÍ ROUTY - PŘI VÝVOJI
+$routes->get('logAD', 'Auth::loginOAUH'); //routa pro tlačítko pro přesměrování na login Microsoft office - OAUH      
+$routes->get('/', 'Auth::callback'); // Routa pro zpracování údajů po přihlášení (Microsoft office - OAUH)   
+$routes->get('azure-users', 'UserAzureSync::index'); //routa pro btn - pro script (načtení všech uživatelů z db - azure)
