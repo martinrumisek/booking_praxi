9c8f607294aa8c17d02e081029fb4e22b81c5307
martinrumisek <mrumisek@seznam.cz>
2024-11-17 18:51:11 +0100

    Vytvořené potřebné routy (přihlašování, zpracovaní dat uživatele a načtení všech uživatelů) pro přesměrování.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,10 +5,16 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/home', 'Home::index');
 $routes->get('login', 'Home::login');
 $routes->get('registration', 'Home::registration');
 $routes->get('practise_offer', 'Home::offerView');
 $routes->get('people','Home::people');
 $routes->get('company','Home::companyView');
 $routes->get('profile','Home::profileView');
+
+
+//Zkoušky
+$routes->get('logAD', 'Auth::loginOAUH');      
+$routes->get('/', 'Auth::callback');   
+$routes->get('azure-users', 'UserAzureSync::index');
