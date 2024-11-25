5e28816e0b9e9fb2a5d27498387eefda82bf7113
martinrumisek <mrumisek@seznam.cz>
2024-11-25 18:22:32 +0100

    Přidaná routa pro navýšení ročníků na nový školní rok, přeřazení tříd.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,9 +16,9 @@ $routes->get('company','Home::companyView'); //stránka pro zobrazení firem
 $routes->get('profile','Home::profileView'); //stránka pro zobrazení profilu
 //ROUTY PRO BTN - SCRIPT
 //ROUTY PRO AUTH
-
-//ZKUŠEBNÍ ROUTY - PŘI VÝVOJI
 $routes->get('logAD', 'Auth::loginOAUH'); //routa pro tlačítko pro přesměrování na login Microsoft office - OAUH      
+$routes->get('logout','Auth::logOut'); //Odhlášení pro uživetele přihlášených přes microsoft office = OAUH uživatel
+//ZKUŠEBNÍ ROUTY - PŘI VÝVOJI
 $routes->get('/', 'Auth::callback'); // Routa pro zpracování údajů po přihlášení (Microsoft office - OAUH)   
-$routes->get('azure-users', 'UserAzureSync::index'); //routa pro btn - pro script (načtení všech uživatelů z db - azure)
-$routes->get('logout','Auth::logOut');
+$routes->get('azure-users', 'UserAzureSync::getAllUsers'); //routa pro btn - pro script (načtení všech uživatelů z db - azure)
+$routes->get('plus-graduationClass', 'UserAzureSync::updateClassYearGraduation');
