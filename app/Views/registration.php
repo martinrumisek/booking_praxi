fdf1e3186e2acdfbe2df0c7afc851d74afaac5df
martinrumisek <mrumisek@seznam.cz>
2024-11-26 20:21:41 +0100

    Úprava registrovacích formulářů. Byla potřeba přidat potřebné kontroly (zatím validace hesla, nastavení min. složitosti hesla, nastavení validního e-mailu.) Je potřeba přidat validaci iča, tak aby splňovalo délku. Není nastaveno aby se formulář neodeslal, když není všechno splňeno.

--- app/Views/registration.php
+++ app/Views/registration.php
@@ -104,6 +104,31 @@
     .login-oauh-block-company{
         display: none;
     }
+    .toggle-pass1, .toggle-pass2 {
+      position: absolute;
+      top: 50%;
+      right: 10px;
+      transform: translateY(-50%);
+      background: none;
+      border: none;
+      cursor: pointer;
+      font-size: 18px;
+    }
+    .invalid-input {
+      border: 0.5px solid red !important;
+    }
+    .requirement {
+      color: red;
+    }
+    .requirement.valid {
+      color: green !important;
+    }
+    .tooltip-inner {
+      max-width: none !important;
+      width: auto;  
+      background-color: white;
+      color: red;            
+    }
     input::placeholder{
         font-size: 20px;
     }
@@ -142,7 +167,7 @@
             <div class="d-flex justify-content-center title-registration "><h2>Registrace</h2></div>
                 <!--<div class="d-flex justify-content-center"><p>pro firmy</p></div>-->
                 <div class="mt-1 container d-flex justify-content-center">
-                    <form action="" style="width: 80%;">
+                    <form action="" style="width: 80%;" novalidate>
                         <div class="mb-3 mt-2 form-floating">
                             <input type="text" id="name_company" class="form-control text-center registration-input shadow" placeholder="Název firmy/instituce" name="nameCompany" >
                             <label for="name_company" class="h6">Název firmy/instituce</label>
@@ -156,12 +181,14 @@
                             <label for="mail" class="h6">E-mail zastupující os.</label>
                         </div>
                         <div class="mb-3 form-floating">
-                            <input type="password" id="passw1" class="form-control text-center registration-input shadow" placeholder="Heslo" name="passwd1">
+                            <input type="password" id="passw1" class="form-control text-center registration-input shadow" placeholder="Heslo" name="passwd1" data-bs-toggle="tooltip" data-bs-placement="bottom">
                             <label for="passw1" class="h6">Heslo</label>
+                            <button type="button" class="toggle-pass1"><i class="fa-regular fa-eye" id="toggle-icon1"></i></button>
                         </div>
                         <div class="mb-3 form-floating">
-                            <input type="password" id="passw2" class="form-control text-center registration-input shadow" placeholder="Potvrzení hesla" name="passwd2">
+                            <input type="password" id="passw2" class="form-control text-center registration-input shadow" placeholder="Potvrzení hesla" name="passwd2" data-bs-toggle="tooltip" data-bs-placement="bottom">
                             <label for="passw2" class="h6">Potvrzení hesla</label>
+                            <button type="button" class="toggle-pass2"><i class="fa-regular fa-eye" id="toggle-icon2"></i></button>
                         </div>
                         <div class="d-flex justify-content-end"><button type="submit" class="btn form-button mt-3 px-5">Registrovat se</button></div>
                     </form>
@@ -177,4 +204,112 @@
         </div>
     </div>
 </div>
+<script>
+    //nastavení bodů, které heslo musí obsahovat.
+    const requirements = {
+        length: /.{8,}/,
+        uppercase: /[A-Ž]/,
+        lowercase: /[a-ž]/,
+        number: /\d/,
+        special: /[!\"#$%&'()*+,\-./:;<=>?@\[\\\]^_`{|}~]/,
+    };
+    //výraz pro e-mail
+    const emailPattern = /^[a-žA-Ž0-9._-]+@[a-žA-Ž0-9.-]+\.[a-žA-Ž]{2,}$/;
+    
+    const createTooltipContent = (password) => {
+        let content = '<ul id="requirements">';
+        for (const [key, regex] of Object.entries(requirements)) {
+            const isValid = regex.test(password) ? 'valid' : '';
+            content += `<li class="requirement ${isValid}" id="${key}">` +
+                        (key === 'length' ? 'Heslo musí obsahovat min. 8 znaků.' :
+                        key === 'uppercase' ? 'Heslo musí obsahovat min. jedno velké písmeno.' :
+                        key === 'lowercase' ? 'Heslo musí obsahovat min. jedno malé písmeno. ' :
+                        key === 'number' ? 'Heslo musí obsahovat min. jedno číslo.' :
+                        'Heslo musí obsahovat min. jeden speciální znak.') +
+                        `</li>`;
+        }
+        content += '</ul>';
+        return content;
+    };
+    const password1Input = document.getElementById('passw1');
+    const password2Input = document.getElementById('passw2');
+    const mailInput = document.getElementById('mail');
+    const checkEmailValidity = () => {
+        const mail = mailInput.value;
+        if (!emailPattern.test(mail)) {
+            mailInput.classList.add('invalid-input');
+        } else {
+            mailInput.classList.remove('invalid-input');
+        }
+    };
+    mailInput.addEventListener('input', checkEmailValidity);
+    const tooltip = new bootstrap.Tooltip(password1Input, {
+        html: true,
+        title: () => createTooltipContent(password1Input.value),
+    });
+    const validatePassword = () => {
+        const password = password1Input.value;
+        const allValid = Object.values(requirements).every((regex) => regex.test(password));
+        if (password && !allValid) {
+            password1Input.classList.add('invalid-input');
+        } else {
+            password1Input.classList.remove('invalid-input');
+        }
+        tooltip.setContent({'.tooltip-inner': createTooltipContent(password),});
+    };
+    
+    password1Input.addEventListener('input', validatePassword);
+    password1Input.addEventListener('focus', validatePassword);
+    password1Input.addEventListener('blur', validatePassword);
+
+    const tooltip2 = new bootstrap.Tooltip(password2Input, {
+        html: true,
+        title: "Hesla se neshodují",
+        trigger: "auto" // Tooltip bude zobrazen manuálně
+    });
+
+    // Kontrola hesel, jestli se shodují
+    const checkPasswordsMatch = () => {
+        const password1 = password1Input.value;
+        const password2 = password2Input.value;
+    
+        if (password1 && password2 && password1 !== password2) {
+            tooltip2.show();
+            password2Input.classList.add('invalid-input');
+        } else {
+            tooltip2.hide();
+            password2Input.classList.remove('invalid-input');
+        }
+    };
+    password1Input.addEventListener('input', checkPasswordsMatch);
+    password2Input.addEventListener('input', checkPasswordsMatch);
+
+    //Zobrazení hesla pomocí ikonky a zároveň měnění ikonky podle situace.
+    const togglePassword1Button = document.querySelector('.toggle-pass1');
+    const togglePassword2Button = document.querySelector('.toggle-pass2');
+    const toggleIcon1 = document.getElementById('toggle-icon1');
+    const toggleIcon2 = document.getElementById('toggle-icon2');
+    togglePassword1Button.addEventListener('click', () => {
+        if (password1Input.type === 'password') {
+        password1Input.type = 'text';
+        toggleIcon1.classList.remove('fa-eye');
+        toggleIcon1.classList.add('fa-eye-slash');
+        } else {
+        password1Input.type = 'password';
+        toggleIcon1.classList.remove('fa-eye-slash');
+        toggleIcon1.classList.add('fa-eye');
+        }
+    });
+    togglePassword2Button.addEventListener('click', () => {
+        if (password2Input.type === 'password') {
+        password2Input.type = 'text';
+        toggleIcon2.classList.remove('fa-eye');
+        toggleIcon2.classList.add('fa-eye-slash');
+        } else {
+        password2Input.type = 'password';
+        toggleIcon2.classList.remove('fa-eye-slash');
+        toggleIcon2.classList.add('fa-eye');
+        }
+    });
+</script>
 <?= $this->endSection() ?>
