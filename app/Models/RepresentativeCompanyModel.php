a4db12be77381b43eeb641d49ef0de4058bd2482
martinrumisek <mrumisek@seznam.cz>
2024-12-01 16:57:06 +0100

    Vytvořen model pro zástupce firmy.

---
+++ app/Models/RepresentativeCompanyModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class RepresentativeCompanyModel extends Model
+{
+    protected $table = 'representative_company';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['name', 'surname', 'mail', 'password', 'phone','function','create_time','edit_time'];
+    protected $useTimestamps = true;
+    protected $createdField = 'create_time'; 
+    protected $updatedField = 'edit_time';  
+    protected $dateFormat = 'datetime';    
+}
