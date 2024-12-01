87c34692fbbc84137785e5a07abb8c80b89dca78
martinrumisek <mrumisek@seznam.cz>
2024-12-01 17:07:12 +0100

    Úprava modelu kvůli špatnému cizímu klíči.

--- app/Models/CompanyModel.php
+++ app/Models/CompanyModel.php
@@ -8,7 +8,7 @@ class CompanyModel extends Model
 {
     protected $table = 'company';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['name', 'ico', 'subject', 'legal_form', 'decription','city','agree_document','street','post_code','create_time','edit_time','Representative_company_id'];
+    protected $allowedFields = ['name', 'ico', 'subject', 'legal_form', 'decription','city','agree_document','street','post_code','create_time','edit_time'];
     protected $useTimestamps = true;
     protected $createdField = 'create_time'; 
     protected $updatedField = 'edit_time';  
