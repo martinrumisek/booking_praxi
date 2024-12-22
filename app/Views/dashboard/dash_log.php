c25864201a3838e1ded7841dda48773c420853da
martinrumisek <mrumisek@seznam.cz>
2024-12-22 13:18:33 +0100

    Úprava na základě úpravy controlleru, přídání jméno přihlášenoho uživatele + ikonka přihlášení / odhlášení.

--- app/Views/dashboard/dash_log.php
+++ app/Views/dashboard/dash_log.php
@@ -15,7 +15,7 @@
     }
 </style>
 <div class="container-fluid">
-    <h2>Přehled log</h2>
+    <h2>Historie přihlášených uživatelů</h2>
     <div class="d-flex m-3">
         <input class="search-input p-2 form-control" type="text" placeholder="Vyhledat">
     </div>
@@ -41,19 +41,16 @@
                     foreach($logs as $log){
                     ?>
                       <tr>
-                        <th class="nowrap" scope="row"><?= $log['name']?></th>
+                        <th class="nowrap" scope="row"><?php if($log['name'] == 'Přihlášení'){echo '<i class="fa-solid fa-key"></i>  ';}else{echo '<i class="fa-solid fa-right-to-bracket"></i>  ';} ?><?= $log['name']?></th>
                         <td>
                             <?= $log['ip_adrese'] ?>
                         </td>
                         <td>
-                            <?= $log['create_time'] ?>
+                            <?= date('d.m.Y H:m:s', strtotime($log['create_time'])) ?>
                         </td>
-                        <td><?= $log['Representative_company_id'] ?? $log['User_id']?></td>
+                        <td><?= $log['user']['name'] . ' ' . $log['user']['surname']?><?php if(!empty($log['user']['Company_id'])){echo ' (firma)';} ?></td>
                     </tr>  
                     <?php }?>
-                    <tr>
-                        <td colspan="7" ><a href="#"><i class="fa-solid fa-plus"></i> Přidat nový termín pro praxi</a></td>
-                    </tr>
                 </tbody>
             </table>
         </div>
