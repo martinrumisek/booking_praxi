84c5d592c6fb3876bb92db0a730a6d51ec8a9225
martinrumisek <mrumisek@seznam.cz>
2024-12-28 17:37:16 +0100

    Úprava stránky, přidání hledání + seřadit. (úprava grafické části) - (potřeba dodělat funkčnost řazení + hledání)

--- app/Views/dashboard/dash_log.php
+++ app/Views/dashboard/dash_log.php
@@ -10,18 +10,49 @@
         background-color: white;
         box-shadow: 0px 3px 6px #00000029;
     }
+    .search-input:focus{
+        border:1px solid #006DBC;
+        outline: none;
+    }
     tr{
         white-space: nowrap;
     }
+    body .table{
+        background-color: black !important;
+    }
+    .btn-search{
+        margin-left: 10px;
+        border-radius: 100%;
+        background-color: white;
+        box-shadow: 0px 3px 6px #00000029;
+    }
+    .btn-search:hover{
+        background-color: #006DBC;
+        color: white;
+    }
 </style>
 <div class="container-fluid">
-    <h2>Historie přihlášených uživatelů</h2>
-    <div class="d-flex m-3">
-        <input class="search-input p-2 form-control" type="text" placeholder="Vyhledat">
+    <h2>Historie aktivity uživatelů</h2>
+    <form action="" method="POST">
+    <div class="d-flex flex-wrap justify-content-between m-3">
+            <div class="d-flex">
+                <input class="search-input p-2 mt-2" id="search-input" type="text" placeholder="Vyhledat uživatele">
+                <button class="btn btn-search mt-2"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+            <div>
+                <select class="search-input mt-2" id="">
+                    <option selected disabled>Seřadit podle</option>
+                    <option value="1">Od přihlášení po odhlášení</option>
+                    <option value="2">Od odhlášení po přihlášení</option>
+                    <option value="3">Datum sestupně</option>
+                    <option value="4">Datum vzestupně</option>
+                </select>
+            </div>
     </div>
+    </form>
     <div class="container-fluid">
         <div class="table-responsive">
-            <table class="table table-striped table-sm">
+            <table class=" table table-sm table-striped">
                 <thead>
                     <tr>
                         <th scope="col" class="nowrap">Akce</th>
@@ -30,7 +61,7 @@
                         <th scope="col">Uživatel</th>
                     </tr>
                 </thead>
-                <tbody class="table-group-divider">
+                <tbody class="table-group-divider" id="log-table">
                     <?php 
                     if(empty($logs)){?>
                         <tr>
@@ -46,7 +77,7 @@
                             <?= $log['ip_adrese'] ?>
                         </td>
                         <td>
-                            <?= date('d.m.Y H:m:s', strtotime($log['create_time'])) ?>
+                            <?= date('d.m.Y H:i:s', strtotime($log['create_time'])) ?>
                         </td>
                         <td><?= $log['user']['name'] . ' ' . $log['user']['surname']?><?php if(!empty($log['user']['Company_id'])){echo ' (firma)';} ?></td>
                     </tr>  
@@ -54,6 +85,36 @@
                 </tbody>
             </table>
         </div>
+        <div class="d-flex justify-content-center"><?= $pager->links() ?></div>
     </div>
 </div>
+<script>
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('search-input');
+    const rows = document.querySelectorAll('#log-table tr');
+    const storedValue = localStorage.getItem('searchValue');
+    if (storedValue) {
+        searchInput.value = storedValue;
+        filterRows(storedValue);
+    }
+    searchInput.addEventListener('keyup', function () {
+        const searchValue = this.value.toLowerCase();
+        localStorage.setItem('searchValue', searchValue);
+        filterRows(searchValue);
+    });
+    function filterRows(searchValue) {
+        rows.forEach(row => {
+            const nameCell = row.querySelector('td:nth-child(4)');
+            if (nameCell) {
+                const name = nameCell.textContent.toLowerCase();
+                if (name.includes(searchValue)) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            }
+        });
+    }
+});
+</script>
 <?= $this->endSection() ?>
