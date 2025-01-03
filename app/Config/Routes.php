745cda16727685dcb2d989a6eeb5388b5f47d1ac
martinrumisek <mrumisek@seznam.cz>
2025-01-03 17:56:13 +0100

    Tvorba potřebných rout, pro administraci firem a zapomenutého hesla.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,6 +9,7 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('login', 'Home::login'); //Přihlašovácí stránka
 $routes->get('registration', 'Home::registration'); //stránka pro registraci firmy
 $routes->get('next-step-register','Auth::continuationRegister' );
+$routes->get('reset-password', 'Auth::resetPassword');
 // ROUTY PRO VIEWČKA
 $routes->get('/home-student', 'Home::index', ['filter' => 'role:student']); //routa na hlavní stránku
 $routes->get('practise_offer', 'Home::offerView'); //stránka pro nabídky praxe
@@ -37,7 +38,16 @@ $routes->get('/delete-category-skill/(:num)', 'Dashboard::deleteCategorySkill/$1
 $routes->get('/delete-skill/(:num)', 'Dashboard::deleteSkill/$1', ['filter' => 'role:admin,spravce']); //! Je potřeba udělat změna kvůli bezpečnosti, bude se muset přidat fetch přes javascript!! -- zatím provyzorně
 $routes->post('/add-practiseManager', 'Dashboard::addPractiseManager' , ['filter' => 'role:admin,spravce']);
 $routes->post('/edit-practiseManager', 'Dashboard::editPractiseManager', ['filter' => 'role:admin,spravce']);
-$routes->get('/postEmail', 'Dashboard::sentEmail');
+$routes->post('/edit-representativeCompany', 'Dashboard::editRepresentativeCompany', ['filter' => 'role:admin,spravce']);
+$routes->post('/add-representativeCompany', 'Dashboard::addRepresentativeCompany', ['filter' => 'role:admin,spravce']);
+$routes->post('/edit-company', 'Dashboard::editCompany', ['filter' => 'role:admin,spravce']);
+$routes->post('/add-new-company', 'Dashboard::addNewCompany', ['filter' => 'role:admin,spravce']);
+$routes->post('/edit-user-password', 'Dashboard::editUserPassword', ['filter' => 'role:admin,spravce']);
+$routes->get('/delete-company/(:num)', 'Dashboard::deleteCompany/$1', ['filter' => 'role:admin,spravce']);
+$routes->get('/delete-representativeCompany/(:num)', 'Dashboard::deleteRepresentativeCompany/$1', ['filter' => 'role:admin,spravce']);
+$routes->get('/delete-practiseManager/(:num)', 'Dashboard::deletePractiseManager/$1', ['filter' => 'role:admin,spravce']);
+
+//$routes->get('/postEmail', 'Dashboard::sentEmail');
 
 //ROUTY PRO AUTH
 $routes->get('logAD', 'Auth::loginOAUH'); //routa pro tlačítko pro přesměrování na login Microsoft office - OAUH      
@@ -47,5 +57,7 @@ $routes->post('/confirmRegister','Auth::completionRegister'); //Routa, která zp
 $routes->post('/loginCompany','Auth::loginCompany'); //Přihlašovací routa pro firmy.
 $routes->get('/logOutCompany', 'Auth::logOutCompany', ['filter' => 'role:company']); //Odhlášení firmy. //! Je potřeba změnit odhlašování na metodu POST.
 $routes->get('/', 'Auth::callback'); // Routa pro zpracování údajů po přihlášení (Microsoft office - OAUH)   //!Změnit routu, ale je potřeba změnit v microsoft Azure zpětnou url
+$routes->post('/new-password', 'Auth::newPassword');
+$routes->post('/forgot-password', 'Auth::forgotPassword');
 //ZKUŠEBNÍ ROUTY - PŘI VÝVOJI
 
