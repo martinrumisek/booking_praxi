4068b935075e0f764d2ae0f3b72313dae71f78d1
martinrumisek <mrumisek@seznam.cz>
2025-01-16 18:27:46 +0100

    Úprava modelu, z důvodu špatných atributů.

--- app/Models/SocialLink_User.php
+++ app/Models/SocialLink_User.php
@@ -6,12 +6,12 @@ use CodeIgniter\Model;
 
 class SocialLink_User extends Model{
     protected $table = 'social_link_has_user';
-    protected $primaryKey = 'social_user_id';
-    protected $allowedFields = ['Social_link_social_id','User_user_id','social_user_url','social_user_create_time','social_user_edit_time', 'social_user_del_time'];
+    protected $primaryKey = 'user_social_id';
+    protected $allowedFields = ['Social_link_social_id','User_user_id','user_social_url','user_social_create_time','user_social_edit_time', 'user_social_del_time'];
     protected $useTimestamps = true;
     protected $useSoftDeletes = true;
-    protected $createdField = 'social_user_create_time'; 
-    protected $updatedField = 'social_user_edit_time';  
-    protected $deletedField = 'social_user_del_time';
+    protected $createdField = 'user_social_create_time'; 
+    protected $updatedField = 'user_social_edit_time';  
+    protected $deletedField = 'user_social_del_time';
     protected $dateFormat = 'datetime';
 }
