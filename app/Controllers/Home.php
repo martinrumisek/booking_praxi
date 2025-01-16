64473d1c5f787ef15533f3b1f6cb3c3f259a2dd6
martinrumisek <mrumisek@seznam.cz>
2025-01-16 18:25:39 +0100

    Zobrazení profilu žáka a jeho editace.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -19,6 +19,8 @@ use App\Models\LogUser;
 use App\Models\CategorySkill;
 use App\Models\Skill;
 use App\Models\User_OfferPractise;
+use App\Models\SocialLink;
+use App\Models\SocialLink_User;
 class Home extends BaseController
 {
     var $session;
@@ -41,6 +43,8 @@ class Home extends BaseController
     var $userSession;
     var $companyUser;
     var $user_skill;
+    var $socialLink;
+    var $socialLink_user;
     public function __construct(){
         $this->session = session();
         $this->userSession = $this->session->get('user');
@@ -62,6 +66,9 @@ class Home extends BaseController
         $this->practiseManagerModel = new PractiseManager();
         $this->offerPractise = new OfferPractise();
         $this->user_skill = new User_Skill();
+        $this->socialLink = new SocialLink();
+        $this->socialLink_user = new SocialLink_User();
+        $this->user_skill = new User_Skill();
     }
     public function homeStudent(){
         $user = $this->userModel->find($this->userSession['id']);
@@ -79,7 +86,7 @@ class Home extends BaseController
         $practises = $this->practiseModel->findAll();
         $count['practise'] = 0;
         foreach($practises as $practise){
-            if($nowDate <= $practise['date_end_new_offer']){
+            if($nowDate <= $practise['practise_end_new_offer']){
                 $count['practise'] = $count['practise'] + 1;
             }
         }
@@ -147,6 +154,7 @@ class Home extends BaseController
         $id = $this->userSession['id'];
         $user = $this->userModel->where('User.user_id', $id)->join('Class', 'User.Class_class_id = Class.class_id AND Class.class_del_time IS NULL')->join('Field_study', 'Class.Field_study_field_id = Field_study.field_id AND Field_study.field_del_time IS NULL')->first();
         $resultCategoryes = $this->categorySkill->join('Skill', 'Category_skill.category_id = Skill.Category_skill_category_id AND Skill.skill_del_time IS NULL')->join('User_has_Skill', 'Skill.skill_id = User_has_Skill.Skill_skill_id AND User_has_Skill.user_skill_del_time IS NULL')->where('User_has_Skill.User_user_id', $id)->find();
+        $socialLinks = $this->socialLink_user->where('User_user_id', $id)->join('Social_link', 'Social_link_has_User.Social_link_social_id = Social_link.social_id AND Social_link.social_del_time IS NULL')->find();
         $categoryes = [];
         foreach ($resultCategoryes as $result){
             $categoryId = $result['category_id'];
@@ -169,6 +177,7 @@ class Home extends BaseController
             'title' => 'Profil',
             'user' => $user,
             'categoryes' => $categoryes,
+            'socialLinks' => $socialLinks,
         ];
         return view ('profile', $data);
     }
@@ -176,6 +185,7 @@ class Home extends BaseController
         $id = $idUser;
         $user = $this->userModel->where('User.user_id', $id)->join('Class', 'User.Class_class_id = Class.class_id AND Class.class_del_time IS NULL')->join('Field_study', 'Class.Field_study_field_id = Field_study.field_id AND Field_study.field_del_time IS NULL')->first();
         $resultCategoryes = $this->categorySkill->join('Skill', 'Category_skill.category_id = Skill.Category_skill_category_id AND Skill.skill_del_time IS NULL')->join('User_has_Skill', 'Skill.skill_id = User_has_Skill.Skill_skill_id AND User_has_Skill.user_skill_del_time IS NULL')->where('User_has_Skill.User_user_id', $id)->find();
+        $socialLinks = $this->socialLink_user->where('User_user_id', $id)->join('Social_link', 'Social_link_has_User.Social_link_social_id = Social_link.social_id AND Social_link.social_del_time IS NULL')->find();
         $categoryes = [];
         foreach ($resultCategoryes as $result){
             $categoryId = $result['category_id'];
@@ -198,7 +208,146 @@ class Home extends BaseController
             'title' => 'Profil',
             'user' => $user,
             'categoryes' => $categoryes,
+            'socialLinks' => $socialLinks,
         ];
         return view ('profile', $data);
+    }
+    public function editProfileView($idUser){
+        $id = $idUser;
+        $role = $this->session->get('role');
+        $userSession = $this->session->get('user');
+        $isAdmin = in_array('admin', $role);
+        $isSpravce = in_array('spravce', $role);
+        if(!$isAdmin && !$isSpravce && $userSession['id'] !== $id){
+            $previousUrl = $this->request->getServer('HTTP_REFERER');
+            if ($previousUrl) {
+                return redirect()->to($previousUrl);
+            } else {
+                return redirect()->to('/home-student');
+            }
+        }
+        $user = $this->userModel->where('User.user_id', $id)->join('Class', 'User.Class_class_id = Class.class_id AND Class.class_del_time IS NULL')->join('Field_study', 'Class.Field_study_field_id = Field_study.field_id AND Field_study.field_del_time IS NULL')->first();
+        $socialLinks = $this->socialLink->join('Social_link_has_User', 'Social_link.social_id = Social_link_has_User.Social_link_social_id AND Social_link_has_User.user_social_del_time IS NULL AND Social_link_has_User.User_user_id = '. $id, 'left')->find();
+        $resultCategoryes = $this->categorySkill->join('Skill', 'Category_skill.category_id = Skill.Category_skill_category_id AND Skill.skill_del_time IS NULL')->join('User_has_Skill', 'Skill.skill_id = User_has_Skill.Skill_skill_id AND User_has_Skill.User_user_id = '. $id. ' AND User_has_Skill.user_skill_del_time IS NULL', 'left')->find();
+        $categoryes = [];
+        foreach ($resultCategoryes as $result){
+            $categoryId = $result['category_id'];
+            if(!isset($categoryes[$categoryId])){
+                $categoryes[$categoryId] = [
+                    'category_id' => $categoryId,
+                    'category_name' => $result['category_name'],
+                    'category_description' => $result['category_description'],
+                    'skills' => [],
+                ];
+            }
+            if(empty($result['user_skill_id'])){$checkUserSkill = 0;}else{$checkUserSkill = 1;}
+            $categoryes[$categoryId]['skills'][] = [
+                'skill_id' => $result['skill_id'],
+                'skill_name' => $result['skill_name'],
+                'skill_description' => $result['skill_description'],
+                'check_user_skill' => $checkUserSkill,
+            ];
+
+        }
+        $data = [
+            'title' => 'Profil',
+            'user' => $user,
+            'categoryes' => $categoryes,
+            'socialLinks' => $socialLinks,
+        ];
+        return view ('edit_profile', $data);
+    }
+    public function editProfile(){
+        $id = $this->request->getPost('idUser');
+        $phone = $this->request->getPost('phone');
+        $birthday = $this->request->getPost('birthday');
+        $socialLinks = $this->request->getPost('socialLinks');
+        $skills = $this->request->getPost('skills');
+        $description = $this->request->getPost('description');
+        if(empty($birthday)){
+            $birthday = null;
+        }
+        $infoUser = [
+            'user_phone' => $phone,
+            'user_date_birthday' => $birthday,
+            'user_description' => $description,
+        ];
+        $this->userModel->update($id, $infoUser);
+        $existingLinks = $this->socialLink_user->where('User_user_id', $id)->withDeleted()->find();
+        $existingId = array_column($existingLinks, 'Social_link_social_id');
+        foreach($socialLinks as $idLink => $link){
+            if(empty($link)){
+                if(in_array($idLink, $existingId)){
+                    $currentId = $existingLinks[array_search($idLink, $existingId)]['user_social_id'];
+                    $updateData = [
+                        'user_social_url' => '',
+                    ];
+                    $this->socialLink_user->update($currentId, $updateData);
+                    $this->socialLink_user->delete($currentId);
+                    continue;
+                }
+            }
+            if(in_array($idLink, $existingId)){
+                $currentUrl = $existingLinks[array_search($idLink, $existingId)]['user_social_url'];
+                if($currentUrl !== $link){
+                    $currentId = $existingLinks[array_search($idLink, $existingId)]['user_social_id'];
+                    $updateData = [
+                        'Social_link_social_id' => $idLink,
+                        'User_user_id' => $id,
+                        'user_social_url' => $link,
+                        'user_social_del_time' => null,
+                    ];
+                    $this->socialLink_user->update($currentId, $updateData);
+                }
+            }else{
+                if(!empty($link)){
+                    $newData = [
+                        'Social_link_social_id' => $idLink,
+                        'User_user_id' => $id,
+                        'user_social_url' => $link,
+                        'user_social_del_time' => null,
+                    ];
+                    $this->socialLink_user->insert( $newData);
+                }
+            }
+        }   
+        $existingSkills = $this->user_skill->where('User_user_id', $id)->withDeleted()->find();
+        $existingIdSkill = array_column($existingSkills, 'Skill_skill_id');
+        if(!empty($skills)){
+            foreach($skills as $skill){
+                if(in_array($skill, $existingIdSkill)){
+                    $currentDelTime = $existingSkills[array_search($skill, $existingIdSkill)]['user_skill_del_time'];
+                    $currentId = $existingSkills[array_search($skill, $existingIdSkill)]['user_skill_id'];
+                    if(!empty($currentDelTime)){
+                        $this->user_skill->update($currentId, ['user_skill_del_time' => null]);
+                    }
+                }else{
+                    $newDataSkill = [
+                        'User_user_id' => $id,
+                        'Skill_skill_id' => $skill,
+                    ];
+                    $this->user_skill->insert($newDataSkill);
+                }
+            }
+        }
+        foreach ($existingSkills as $existingSkill) {
+            if(!empty($skills)){
+                if (!in_array($existingSkill['Skill_skill_id'], $skills)) {
+                    $this->user_skill->delete($existingSkill['user_skill_id']);
+                }
+            }else{
+                $this->user_skill->delete($existingSkill['user_skill_id']);
+            }
+        }
+
+
+
+        $previousUrl = $this->request->getServer('HTTP_REFERER');
+            if ($previousUrl) {
+                return redirect()->to($previousUrl);
+            } else {
+                return redirect()->to('/home-student');
+            }
+
     }
 }
