44d23510277a3d6880888c96b9a78aa07de34c07
martinrumisek <mrumisek@seznam.cz>
2025-02-11 08:11:15 +0100

    Přídán validační hláška pro přihlašování.

--- app/Views/layout/layout.php
+++ app/Views/layout/layout.php
@@ -3,5 +3,16 @@
 <?= $this->include('layout/head')?>
 <body>
 <?= $this->renderSection('content')?>
+<?= view('layout/errModal') ?>
+<?php if ($error = session()->getFlashdata('err_message')){ ?>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var errorMessage = "<?= esc($error); ?>"; 
+            document.getElementById('errorMessage').textContent = errorMessage;
+            var errorModal = new bootstrap.Modal(document.getElementById('errorModal'));
+            errorModal.show();
+        });
+    </script>
+<?php }?>  
 </body>
 </html>
