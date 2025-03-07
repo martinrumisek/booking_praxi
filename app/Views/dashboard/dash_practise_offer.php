aab94f6e715e810f8b023e1e1bce2961e2038c51
martinrumisek <mrumisek@seznam.cz>
2025-03-07 09:36:36 +0100

    Úprava layoutu v části administrace nabídky praxe

--- app/Views/dashboard/dash_practise_offer.php
+++ app/Views/dashboard/dash_practise_offer.php
@@ -2,11 +2,47 @@
 
 <?= $this->section('content') ?>
 <style>
-
+.icon-edit{
+    margin-left: 2px;
+    margin-right: 2px;
+}
+.icon-delete:hover{
+    color: red;
+}
+.icon-repair:hover{
+    color: gray;
+}
+.icon-show:hover{
+    color: gray;
+}
+.btn-table-remove-user{
+    margin-left: 5px;
+    color: white;
+    padding-left: 3px;
+    padding-right: 3px;
+    background-color: #006DBC;
+    box-shadow: 0px 3px 6px #00000029;
+    border-radius: 5px;
+}
+.btn-table-remove-user:hover{
+    background-color: red;
+    color: white;
+}
+.btn-add-practise{
+    box-shadow: 0px 3px 6px #00000029;
+    background-color: white;
+    padding: 8px;
+    border-radius: 8px; 
+    color: black;
+}
+.btn-add-practise:hover{
+    background-color: #006DBC;
+    color: white;
+}
 </style>
-<div class="d-flex flex-wrap justify-content-between">
+<div class="d-flex flex-wrap justify-content-between align-items-center">
 <div class="m-4 d-flex flex-wrap align-items-center"><h5 class="m-1"><?php echo $practise['practise_name'];?></h5><p class="m-1"><?php echo '(';  $count = count($dates); foreach($dates as $date){echo date('d.m.Y', strtotime($date['date_date_from'])) . ' - ' . date('d.m.Y', strtotime($date['date_date_to'])); if($count > 1){echo ' / '; $count--;}} echo ')';?></p></div>
-<div class="m-4"><a href="">Přidat praxi</a></div>
+<div class="m-4"><a class="btn-add-practise" href="">Přidat praxi</a></div>
 </div>
 <div class="d-flex justify-content-between flex-wrap">
     <!--- Zde bude hledání -->
@@ -43,8 +79,8 @@
                         <td><?= $offer['manager_mail'] ?></td>
                         <td><?= $offer['manager_phone'] ?></td>
                         <td><?= $offer['company_name'] . ' (' . $offer['company_ico'] . ')'?></td>
-                        <td><?php if(!empty($offer['user_name'])){echo $offer['user_name'] . ' ' . $offer['user_surname'] . ' (' . $offer['class_class'] . '.' . $offer['class_letter_class'] . ', ' . $offer['field_shortcut'] . ')';}else{echo '<i class="fa-solid fa-xmark"></i>' . ' ' . '<a href="#">Přiřadit</a>';} ?></td>
-                        <td><div class="d-flex justify-content-center align-items-center"><a class="" href=""><i class="fa-solid fa-eye"></i></a><a class="" href=""><i class="fa-solid fa-pencil"></i></a><a class="" href=""><i class="fa-solid fa-trash"></i></a></div></td>
+                        <td><?php if(!empty($offer['user_name'])){echo $offer['user_name'] . ' ' . $offer['user_surname'] . ' (' . $offer['class_class'] . '.' . $offer['class_letter_class'] . ', ' . $offer['field_shortcut'] . ')'; echo '<a href="" class="btn-table-remove-user">Zrušit</a>';}else{echo '<i class="fa-solid fa-xmark"></i>' . ' ' . '<a href="#">Přiřadit</a>';} ?></td>
+                        <td><div class="d-flex justify-content-center align-items-center"><a class="icon-edit icon-show" href="<?=base_url('/practise-offer-view/'. $offer['offer_id'])?>"><i class="fa-solid fa-eye"></i></a><a class="icon-edit icon-repair" href="<?= base_url('/edit-practise-offer-view/'.$offer['offer_id']) ?>"><i class="fa-solid fa-pencil"></i></a><a class="icon-edit icon-delete" href="<?= base_url('/delete-practise-offer/'.$offer['offer_id']) ?>"><i class="fa-solid fa-trash"></i></a></div></td>
                     </tr>  
                     <?php }?>
                 </tbody>
